ad42521db323f4218c3dc3a1eba176efdc169e08
vtorres96 <victorcdc96@gmail.com>
2019-09-18 18:38:23 -0300

    adding database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,7 @@
-a;
+require('dotenv').config();
+
+export const dialect = process.env.DB_PROVIDER;
+export const host = process.env.DB_HOST;
+export const username = process.env.DB_USER;
+export const password = process.env.DB_PASS;
+export const database = process.env.DB_NAME;
