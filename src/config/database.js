977836a227fc6110a332d6812f3fd177969f173f
Veronesiii <felipeveronesi1@gmail.com>
2019-09-19 17:16:27 -0300

    changing config files

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
-export const dialect = process.env.DB_PROVIDER;
-export const host = process.env.DB_HOST;
-export const username = process.env.DB_USER;
-export const password = process.env.DB_PASS;
-export const database = process.env.DB_NAME;
+module.exports = {
+    dialect: process.env.DB_PROVIDER,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+}
+
