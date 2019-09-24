6c1cb2957442bffe92ab1867371ff7be5e33c4bd
Veronesiii <felipeveronesi1@gmail.com>
2019-09-24 17:25:08 -0300

    Users CRUD done

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -2,8 +2,57 @@ const { User } = require('../models/');
 
 module.exports = {
 	async index(req, res) {
-		await User.findAll().then(users => {
-			console.log(users);
-		});
+		const users = await User.findAll();
+		console.log(users);
+	},
+
+	async buscarPorId(req,res) {
+		const id = req.params.id
+		const users = await User.findByPk(id);
+		console.log(users);
+	},
+	
+	async createUser(req,res) {
+		await User.create({
+			firstname: req.body.firstname,
+			lasname: req.body.lastname,
+			nickname: req.body.nickname,
+			image: req.body.image,
+			email: req.body.email,
+			password_hash: req.body.password_hash,
+
+		})
+	},
+
+	async updateUser(req,res) {
+		const id = req.params.id
+		await User.update({
+			firstname: req.body.firstname,
+			lastname: req.body.lastname,
+			nickname: req.body.nickname,
+			image: req.body.image,
+			email: req.body.email,
+			password_hash: req.body.password_hash, 
+		},
+		{
+			where: {
+				id : id
+			},
+		}).then((user)=>{
+			res.send('Usuario alterado com sucesso')
+		}).catch((error)=>{
+			res.send("Algum erro foi encontrado" + error)
+		})
+	},
+
+	async deleteUser(req,res){
+		const id = req.params.id
+		User.destroy({
+			where: { id: id }
+		}).then((user)=> {
+			res.redirect('/');
+		}).catch((error)=>{
+			res.send('Erro ao deletar usuario' + error + id);
+		})
 	}
 };
