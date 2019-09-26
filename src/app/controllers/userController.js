0c867b25be782b0c935bfd5951c650a27c675393
vtorres96 <victorcdc96@gmail.com>
2019-09-26 17:25:20 -0300

    optimizing and creating new methods

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,4 +1,5 @@
 const { User } = require('../models/');
+const bcrypt = require('bcrypt');
 
 module.exports = {
 	async index(req, res) {
@@ -6,53 +7,108 @@ module.exports = {
 		console.log(users);
 	},
 
-	async buscarPorId(req,res) {
-		const id = req.params.id
+	async searchById(req, res) {
+		const id = req.params.id;
 		const users = await User.findByPk(id);
 		console.log(users);
 	},
-	
-	async createUser(req,res) {
-		await User.create({
-			firstname: req.body.firstname,
-			lasname: req.body.lastname,
-			nickname: req.body.nickname,
-			image: req.body.image,
-			email: req.body.email,
-			password_hash: req.body.password_hash,
 
-		})
+	async create(req, res) {
+		const {
+			firstname,
+			lastname,
+			nickname,
+			image,
+			email,
+			password_hash
+		} = req.body;
+
+		const user = await User.create({
+			firstname: firstname,
+			lastname: lastname,
+			nickname: nickname,
+			image: image,
+			email: email,
+			password_hash: bcrypt.hashSync(password_hash, 10)
+		});
+
+		if (user) {
+			if (
+				user.email == email &&
+				bcrypt.compareSync(password_hash, user.password_hash)
+			) {
+				res.redirect('/');
+			}
+		}
 	},
 
-	async updateUser(req,res) {
-		const id = req.params.id
-		await User.update({
-			firstname: req.body.firstname,
-			lastname: req.body.lastname,
-			nickname: req.body.nickname,
-			image: req.body.image,
-			email: req.body.email,
-			password_hash: req.body.password_hash, 
-		},
-		{
-			where: {
-				id : id
+	async validate(req, res) {
+		const { session } = req;
+		const { email, password_hash } = req.body;
+
+		const user = await User.findOne({ where: { email } });
+
+		if (!user) {
+			res.status(400).json({ error: 'Usuário não existe' });
+		}
+
+		if (
+			user.email == email &&
+			bcrypt.compareSync(password_hash, user.password_hash)
+		) {
+			session.logado = {
+				id: user.id,
+				name: user.firstname
+			};
+			res.redirect('/');
+		} else {
+			res.status(400).json({ error: 'Usuário ou senha inválidos' });
+		}
+	},
+
+	async update(req, res) {
+		const id = req.params.id;
+		const {
+			firstname,
+			lastname,
+			nickname,
+			image,
+			email,
+			password_hash
+		} = req.body;
+		await User.update(
+			{
+				firstname: firstname,
+				lastname: lastname,
+				nickname: nickname,
+				image: image,
+				email: email,
+				password_hash: password_hash
 			},
-		}).then((user)=>{
-			res.send('Usuario alterado com sucesso')
-		}).catch((error)=>{
-			res.send("Algum erro foi encontrado" + error)
-		})
+			{
+				where: {
+					id: id
+				}
+			}
+		)
+			.then(user => {
+				res.send('Usuario alterado com sucesso');
+			})
+			.catch(error => {
+				res.send('Algum erro foi encontrado' + error);
+			});
 	},
 
-	async deleteUser(req,res){
-		const id = req.params.id
+	async deleteUser(req, res) {
+		const id = req.params.id;
 		User.destroy({
 			where: { id: id }
-		}).then((user)=> {
-			res.redirect('/');
-		}).catch((error)=>{
-			res.send('Erro ao deletar usuario' + error + id);
 		})
+			.then(user => {
+				res.redirect('/');
+			})
+			.catch(error => {
+				res.send('Erro ao excluir usuario' + error + id);
+			});
 	}
 };
