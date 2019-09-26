35631d5fada3e2812bf4d5c0fed5038d03236b49
vtorres96 <victorcdc96@gmail.com>
2019-09-26 17:22:27 -0300

    setting the session

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 const path = require('path');
-
+const session = require('express-session');
 // importing dotenv with environment variables
 require('dotenv').config();
 
@@ -13,6 +13,16 @@ const http = require('http');
 // instantiating http
 const server = http.createServer(app);
 
+// setting the session
+app.use(
+	session({
+		secret: 'titela',
+		resave: true,
+		saveUninitialized: true,
+		cookie: { secure: true }
+	})
+);
+
 // importing routes
 const routes = require('./routes/routes');
 
@@ -21,11 +31,11 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // setting template engine
 app.engine(
-  'hbs',
-  hbs({
-    defaultLayout: 'master.hbs',
-    extname: '.hbs'
-  })
+	'hbs',
+	hbs({
+		defaultLayout: 'master.hbs',
+		extname: '.hbs'
+	})
 );
 
 // specifying path to views
@@ -34,15 +44,14 @@ app.set('views', path.join(__dirname, './views'));
 // specifying template engine
 app.set('view engine', 'hbs');
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 
 // adding routes
 app.use(routes);
 
 // server listener
 server.listen(process.env.PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Listening on port ${process.env.PORT}`);
+	// eslint-disable-next-line no-console
+	console.log(`Listening on port ${process.env.PORT}`);
 });
